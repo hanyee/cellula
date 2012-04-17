b6fc8f81b04a5108ff364fe74014976787b57212
hanyee <www.hanyee.cn@gmail.com>
2012-04-17 18:44:18 +0800

    add Class file

--- Class.js
+++ Class.js
@@ -1,7 +1,85 @@
 /**
- * Created by JetBrains WebStorm.
  * User: hanyee
- * Date: 12-4-17
- * Time: 下午6:41
+ * Date: 2012-4-17
+ * Time: 17:41
  * To change this template use File | Settings | File Templates.
  */
+
+(function(){
+    this.Class = function(){
+        "use strict";
+
+        var _class = function(){};
+
+        // another way to define fnTest for browsers like firefox that the 'toString' method do not suppot to out put the commented line.
+        // fnTest = /xyz/.test(function () {
+        //     //xyz;
+        // }) ? /\b_super\b/ : /.*/;
+
+        var toString = Object.prototype.toString, objTest = /\bObject\b/,_args = arguments, prototype = _class.prototype, fnTest = /xyz/.test(function () {xyz;}) ? /\b_super\b/ : /.*/ ;
+
+
+        //_class.prototype = typeof _args[0] === 'object'? _args[0] : (typeof _args[1] === 'object' ? _args[1] : {}); //prototype;
+        _class.prototype = objTest.test(toString.call(_args[0])) ? _args[0] : ( objTest.test(toString.call(_args[1])) ? _args[1] : {} );
+        _class.prototype.__className__ = typeof _args[0] === 'string' ? _args[0] : 'unnamed';
+        _class.prototype.constructor = prototype.constructor;
+
+
+
+        //_class.prototype = typeof _args[0] === 'object'? _args[0] : {}; //prototype;
+        //_class.prototype.constructor = prototype.constructor;
+        //_class.prototype.__className__ = _args[1] || 'unnamed';
+
+        _class.inherits = function(parent){
+            if(typeof parent === 'function' && parent.prototype.__className__){
+
+                var _super = parent.prototype, proto = new parent(), prop = this.prototype;
+
+                for(var name in prop){
+                    /*
+                     console.log(name);
+                     console.log(typeof prop[name] === "function");
+                     console.log(typeof _super[name] === "function");
+                     console.log(fnTest.test(prop[name]));
+                     console.log(prop[name].toString());
+                     console.log(_super[name].toString());
+                     console.log('------end----')
+                     */
+                    proto[name] = name !== 'constructor' && typeof prop[name] === "function" && typeof _super[name] === "function" && fnTest.test(prop[name]) ?
+                        (function(name,fn){
+                            return function() {
+                                var tmp = this._super;
+
+                                // Add a new ._super() method that is the same method
+                                // but on the super-class
+                                this._super = _super[name];
+
+                                // The method only need to be bound temporarily, so we
+                                // remove it when we're done executing
+                                var ret = fn.apply(this, arguments);
+                                this._super = tmp;
+
+                                return ret;
+                            };a
+                        })(name, prop[name]) : prop[name];
+
+                }
+
+                this.prototype = proto;
+                //this.prototype.constructor = prop.constructor;
+
+                //console.log(this.prototype);
+                //console.log(_super  );
+            }else{
+                // TODO:
+                throw 'parent class type error!';
+            }
+
+            return this;
+        };
+
+
+        return _class;
+    };
+
+})();
