2c391d063c165cdc54f774cf98170db8318ac0a0
hanyee <www.hanyee.cn@gmail.com>
2012-05-04 15:55:53 +0800

    table added

--- searchingForm/table.js
+++ searchingForm/table.js
@@ -2,22 +2,17 @@
  * Created by JetBrains WebStorm.
  * User: hanyee
  * Date: 12-4-28
- * Time: 下午4:31
+ * Time: 下午4:46
  * To change this template use File | Settings | File Templates.
  */
 
-var SearchingForm = new Class('SearchingForm', {
-    root : null,
-    collection : null,
+var DataTable = new Class('DataTable', {
+    root:null,
     init : function(){
 
         this.registerEvents();
     },
     registerEvents:function(){
 
-    },
-    search : function(){
-
     }
-
 });
