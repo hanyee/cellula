0c257d5bf7920793cac08b537dac8de1a5c4bde1
hanyee <www.hanyee.cn@gmail.com>
2012-05-11 19:16:27 +0800

    add paginator

--- searchingForm/lib/paginator.js
+++ searchingForm/lib/paginator.js
@@ -6,12 +6,36 @@
  * To change this template use File | Settings | File Templates.
  */
 
+
 var Paginator = new Class('Paginator', {
-    pageDefault : {},
+    pageDefault : {
+        size : {},
+        number: {}
+    },
     paginate : function(){
+        console.log('paginate');
+        this.save();
+        console.log(this.getData());
+
+        this.applyInterface('search', this.getData());
 
     },
-    init : function(){
+    getDefault : function(){
+        return this.pageDefault;
+    },
+    init : function(cfg){
+        this.initCfg(cfg);
 
+        this.bindAll('changeSize', 'paginate');
+
+        this.registerEvents();
+    },
+    changeSize : function(){
+        this.save();
+        console.log('change');
+        console.log(this.getData());
+        // TODO:
+        // mix this.getData() && this.pageDefault.number
+        //this.applyInterface('search', mixed data);
     }
 }).inherits(SearchModuleBase);
