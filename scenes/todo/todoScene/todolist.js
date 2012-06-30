0a777a46a06b53ec5da1e7affe6eb9f62cd709ee
hanyee <www.hanyee.cn@gmail.com>
2012-07-01 01:38:02 +0800

    add todo scene

---
+++ scenes/todo/todoScene/todolist.js
@@ -0,0 +1,26 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: hanyee
+ * Date: 12-7-1
+ * Time: 上午12:41
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function(util, Class){
+    this.ToDoList = new Class('ToDoList', {
+        init : function(){
+
+        }
+
+    }).inherits(Cellula.Block);
+})(Cellula._util, Cellula.Class);
+
+
+(function(util, Class){
+    this.ToDoItem = new Class('ToDoItem', {
+        init : function(){
+
+        }
+
+    }).inherits(Cellula.Block);
+})(Cellula._util, Cellula.Class);
---
+++ scenes/todo/todoSceneAlipay/todolist-alipay.js
@@ -0,0 +1,15 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: hanyee
+ * Date: 12-7-1
+ * Time: 上午1:02
+ * To change this template use File | Settings | File Templates.
+ */
+(function(util, Class){
+    this.ToDoListAlipay = new Class('ToDoListAlipay', {
+        init : function(){
+
+        }
+
+    }).inherits(ToDoList);
+})(Cellula._util, Cellula.Class);
