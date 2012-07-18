671892793e7f2e30621397fd3f45c6b411d4c1b1
hanyee <imac.hanyee@alipay>
2012-07-18 15:55:23 +0800

    new pagingtpl added

--- scenes/searchingForm/searchingSceneAlipay/assets-alipay.js
+++ scenes/searchingForm/searchingSceneAlipay/assets-alipay.js
@@ -43,12 +43,28 @@ searchingSceneAssets.pagingTplStyles = [
         +'<select name="size" id="J_size">'
             +'$-{#options}<option $-{#selected}selected $-{/selected}value="$-{num}">$-{num}条</option>$-{/options}'
         +'</select>'
+    +'</div>',
+
+    '<div class="mi-paging fn-right">'
+        +'<a class="mi-paging-prev" href="#">上一页</a>'
+        +'<span class="mi-paging-info">第<span class="mi-paging-bold">$-{current}/$-{totalPages}</span>页</span>'
+        +'<a class="mi-paging-next" href="#">下一页</a>'
+        +'<span class="mi-paging-info fn-ml0"><span class="mi-paging-bold">$-{current}/$-{totalPages}</span>页</span>'
+        +'<span class="mi-paging-info mi-paging-which"><input id="J_number" type="text" value="" name="number"></span>'
+        +'<a href="#" class="mi-paging-info mi-paging-goto"><span>跳转</span></a>'
     +'</div>'
+    + '<div class="page-info-box">'
+        + '共$-{totalItems}条记录，共$-{totalPages}页，当前显示$-{startItem}-$-{endItem}条，每页'
+        + '<select name="size" id="J_size">'
+        + '$-{#options}<option $-{#selected}selected $-{/selected}value="$-{num}">$-{num}条</option>$-{/options}'
+        + '</select>'
+    + '</div>'
 ];
 
 searchingSceneAssets.tableTplStyles = [
     '$-{#rows}<tr$-{#spliter} class="mi-table-spliter"$-{/spliter}>'
         +'$-{#items}<td>$-{content}</td>$-{/items}'
         +'</tr>$-{/rows}',
+
     '<tr>$-{#head}<th>$-{content}</th>$-{/head}</tr>'
 ];
