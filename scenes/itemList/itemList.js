73d4e906b264fe2bee653ec41cd6210bffbd499e
hanyee <imac.hanyee@alipay>
2013-04-27 17:25:05 +0800

    bugfix for branch 0.4.0

--- scenes/itemList/itemList.js
+++ scenes/itemList/itemList.js
@@ -5,9 +5,9 @@
         conf:{},
         editNode:undefined,
         delNode:undefined,
-        init:function(conf){
+        init:function(conf, data){
             this._super(conf);
-            this.rootNode = this.rootNode || this.create();
+            this.rootNode = this.rootNode || this.create(data);
             this.editNode = util.getElementsByClassName(this.conf.edit, this.rootNode)[0];
             this.delNode = util.getElementsByClassName(this.conf.del, this.rootNode)[0];
 
@@ -19,19 +19,17 @@
             this.editNode && (this.editNode.onclick = this.edit);
             this.delNode && (this.delNode.onclick = this.remove);
         },
-        create:function(){
-            util.log('create' +this.__cid__);
-            this.emit('Item:CREATED');
+        create:function(data){
+            // should return the item dom node
+            // this.rootNode = makeSth();
+            // return this.rootNode
         },
         edit:function(e){
             //e = e || window.event;
-            util.log('edit '+this.__cid__);
-            this.emit('Item:EDIT');
+            //util.log('edit '+this.__cid__);
+            //this.emit('Item:EDIT');
         },
-        remove:function(e){
-            util.log('remove '+this.__cid__);
-            this.emit('Item:REMOVE');
-        }
+        remove:function(e){}
     }).inherits(c.Cell);
 
     var ItemList = new Class('ItemList' ,{
@@ -43,7 +41,8 @@
             add:'item-add',
             edit:'item-edit',
             del:'item-del',
-            attribute:'data-itemlist'
+            attribute:'data-itemlist',
+            listNode:'listNode'
         },
         collection:undefined,
         init:function(conf){
@@ -53,11 +52,13 @@
             // obj
             if(util.isObject(conf)) _conf = conf;  //util.mix(_conf, conf);
 
-            if(!util.isObject(conf) || util.isEmpty(_conf)) return;
+            if(util.isEmpty(_conf)) return;
 
             this._super(_conf);
 
             this._parseDomConf(this.rootNode);
+
+            this.listNode = document.getElementById(this.conf.listNode);
 
             this.collection = new Cll({type:this.itemType});
 
@@ -96,24 +97,32 @@
                 this.addItem(itemNode);
             }, this);
         },
-        add:function(e){
-            this.listNode.appendChild(this.addItem().rootNode);
+        add:function(data){
+            var node = this.addItem(data).rootNode;
+            node && node.nodeType === 1 && this.listNode.appendChild(node);
         },
         addItem:function(item){
             if(!item) item = new this.itemType({conf:this.conf});
 
-            if((!this.listNode) && item.rootNode.nodeType === 1) this.listNode = item.rootNode.parentNode;
+            // param item is item's data
+            if(!item.__cid__) item = new this.itemType({conf:this.conf}, item);
 
+            if((!this.listNode) && item.rootNode.nodeType === 1) this.listNode = item.rootNode.parentNode;
             this.follow(item);
             this.collection.push(item);
             return item;
+        },
+        render:function(data){},
+        removeItem:function(item){
+            this.collection.remove(item);
+            this.listNode.removeChild(item.rootNode);
         },
         receiver:function(e){
             if(!e) return;
             var targ = e.target, evt = e.name.split(':')[1];
             switch (evt){
                 case 'REMOVE':
-                    this.listNode.removeChild(targ.rootNode);
+                    this.removeItem(targ);
                     break;
             }
             util.log(e);
